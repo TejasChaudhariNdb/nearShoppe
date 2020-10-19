f863f73672d2bb52cde5458adb59f91c3ebdfe31
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2020-10-19 22:56:13 +0530

    baba ka disign

---
+++ product.php
@@ -0,0 +1,56 @@
+<html>
+<?php include("components/head.php"); ?>
+
+    <body>
+
+   <?php include("components/navbar.php")?>
+
+             
+
+<!--   Search Card Goes HEre -->
+
+
+
+
+<style>
+
+
+
+    </style>
+
+<div class="container">
+
+<div class="carousel">
+    <a class="carousel-item" href="#one!"><img src="https://img.etimg.com/thumb/width-640,height-480,imgsize-789754,resizemode-1,msid-73320353/small-biz/sme-sector/the-kirana-is-a-technology-shop-too/kirana-bccl.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="https://images.newindianexpress.com/uploads/user/imagelibrary/2020/3/25/w1200X800/Lockdown_marginallyz.jpg"></a>
+    <a class="carousel-item" href="#three!"><img src="https://images.moneycontrol.com/static-mcnews/2020/10/Baba-Ka-Dhaba-Zomato-770x433.jpg?impolicy=website&width=770&height=431"></a>
+    <a class="carousel-item" href="#four!"><img src="https://images.livemint.com/img/2020/05/01/600x338/PTI13-04-2020_000051B_1588354728439_1588354749822.jpg"></a>
+  </div>
+
+                                  
+
+
+
+</div>
+
+<script>
+ 
+ $('.sidenav').show();
+
+ 
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+                 </script>
+
+
+    <!-- Aaaj Tevv -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>
+        
